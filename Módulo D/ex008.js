de06b3eb3f9398f42cf1c517383048ec3ca64373
Samuel Fortes Brasil <samueldbz441@gmail.com>
2025-02-09 13:18:51 -0300

    módulo C concluído e Início do Módulo D

---
+++ Módulo D/ex008.js
@@ -0,0 +1,6 @@
+var vel = 30.5;
+console.log(`A velocidade do seu carro é ${vel} Km por Hora!`)
+console.log('Dirija sempre com cinto de segurança')
+if(vel > 40){
+   console.log('A limite de velocidade foi exedido! MULTADO!')
+}
---
+++ Módulo D/ex009.js
@@ -0,0 +1,7 @@
+var media = 3.21
+if(media >= 6){
+    console.log('Aluno Aprovado!')
+}else{
+    var falta = 6 - media
+    console.log(`Aluno reprovado! Ainda faltam ${falta} pontos para atingir a média`)
+}
