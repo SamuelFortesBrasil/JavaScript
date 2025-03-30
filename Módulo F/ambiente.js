bec738d8b77f64bd1484cf236370077d1ae54445
Samuel Fortes Brasil <samueldbz441@gmail.com>
2025-03-29 22:31:40 -0300

    Exercícios Iniciais Módulo F

---
+++ Módulo F/ambiente.js
@@ -0,0 +1,7 @@
+var num = [5,7,8,9,3]
+console.log(`nosso vetor é o ${num}`)
+console.log(`O vetor tem ${num.length} posições!`)
+console.log(`Sua ordem crescente é ${num.sort()}`)
+num.push(21)
+num[6] = 10
+console.log(num)
---
+++ Módulo F/ex18.js
@@ -0,0 +1,7 @@
+var num = [2,3,7,6]
+/*for(let cont = 0 ; cont < num.length ; cont ++){
+    console.log(`O valor: ${num[cont]} está na posição ${cont}`)
+}*/
+for(let c in num){
+    console.log(`O valor: ${num[c]} está na posição ${c}`)
+}
